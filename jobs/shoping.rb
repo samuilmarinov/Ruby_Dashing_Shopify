8678e1220139c261801505cef96505f88c151b1d
Samuil Marinov <samuilmarinov@itg.co.uk>
2020-10-31 14:59:21 +0000

    Initial commit

---
+++ jobs/shoping.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'net/http'
+require 'net/https'
+require 'shopify_api'
+	
+
+login = 'f377251f6fbf4630ab441823f874097a'
+secret = '2444fcf48b9f798f117ca4f427b20054'
+shop = 'morale-patch-armory'
+shop_url = "https://" + login + ":" + secret + "@" + shop + ".myshopify.com/admin"
+	
+	
+SCHEDULER.every '20s' do
+    # make the GET request
+    resp = Net::HTTP.get(URI("http://graph.facebook.com/fql?q=SELECT%20share_count,%20like_count,%20comment_count,%20total_count%20FROM%20link_stat%20WHERE%20url=%22http://www.google.fr%22&format=json"))
+
+    # parse the JSON into a ruby hash
+    json = JSON.parse(resp)
+
+    # pull the like_count value out of the response
+	current_valuation = json["data"][0]["like_count"]
+	
+	ShopifyAPI::Base.site = shop_url
+	shop = ShopifyAPI::Shop.current
+
+	orderCount = ShopifyAPI::Order.count()
+
+	send_event('valuation', { current: current_valuation})
+	send_event('synergy',   { value: orderCount })
+	send_event('vvv', { current: orderCount })
+end
